92265e701a375d21c19b709359e80e1a46264b5b
syedsibtdev <125146972+syedsibtdev@users.noreply.github.com>
2023-02-14 23:05:21 -0800

    Delete java.js

--- java.js
+++
@@ -1,7 +0,0 @@
-/***************** Typing Animation ****************/
-var typed = new Typed(".typing",{
-    strings:[" "," Web Designer","Web Developer","Graphic Designer"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
